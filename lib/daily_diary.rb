bdad6a16bb8a0243f4862eadc5634b1ed347626a
Lorna Harwood <lornapeepwreckers@gmail.com>
2019-05-20 17:52:52 +0100

    stores the new diary entry

--- lib/daily_diary.rb
+++ lib/daily_diary.rb
@@ -1,6 +1,14 @@
+require_relative 'entry'
 
 class DailyDiary
 
+  attr_reader :entries
+
+  def initialize
+    @entries = []
+  end
+
   def new_entry(entry)
+    @entries << entry
   end
 end
--- spec/daily_diary_spec.rb
+++ spec/daily_diary_spec.rb
@@ -6,6 +6,13 @@ describe DailyDiary do
     it 'allows a new entry' do
       expect(subject).to respond_to(:new_entry).with(1).argument
     end
+
+    it 'stores a new entry' do
+      entry = Entry.new
+      subject.new_entry(entry)
+      expect(subject.entries[0]).to eq entry
+    end
+
   end
 
 end
@@ -13,3 +20,8 @@ end
   # As a user
   # So that I can keep a daily diary
   # I want to be able to add a new Diary Entry
+
+
+  # As a user
+  # So that I can identify my entry in future
+  # I want to give each Diary Entry a title
